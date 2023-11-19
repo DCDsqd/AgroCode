8176bddd3e8045a0009c4e9c56ea9001e4d621ab
storst1 <kokakolazzz100@mail.ru>
2023-11-19 14:07:39 +0300

    upd

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from parser import parse
 from normalize import normalize
+from model import clusterize
 
 
 def main():
     parse()
     normalize()
+    clusterize()
 
 
 if __name__ == "__main__":
